5415fb51983254283ef2a068bf7b14cbcced74e4
stiipp <curilansteph456@gmail.com>
2026-02-19 15:00:05 +0800

    Add macro for generating custom schema name

---
+++ dbt/macros/get_custom_schema.sql
@@ -0,0 +1,9 @@
+-- This macro generates a schema name based on the provided custom schema name and the default target schema.
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+        {{ default_schema }}
+    {%- else -%}
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
