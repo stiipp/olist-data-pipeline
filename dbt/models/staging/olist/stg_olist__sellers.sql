71db0e59dd200b40a46cb9986b334c1f66684926
stiipp <curilansteph456@gmail.com>
2026-02-19 14:20:46 +0800

    Add initial staging model for sellers

---
+++ dbt/models/staging/olist/stg_olist__sellers.sql
@@ -0,0 +1,17 @@
+with source as (
+
+    select * from {{ source('olist', 'olist_sellers') }}
+),
+renamed as (
+    select
+        -- Primary Key
+        seller_id,
+        
+        -- Categorical Info
+        seller_zip_code_prefix,
+        seller_city,
+        seller_state
+
+    from source
+)
+select * from renamed
