977c6db591f3e4995033d195aa4512b288667099
stiipp <curilansteph456@gmail.com>
2026-02-20 10:37:05 +0800

    Refactor: added aliases for stg reviews

--- dbt/models/staging/olist/stg_olist__reviews.sql
+++ dbt/models/staging/olist/stg_olist__reviews.sql
@@ -7,10 +7,10 @@ renamed as (
         review_id,
         order_id,
         review_score,
-        review_comment_title,
-        review_comment_message,
-        cast(review_creation_date as timestamp) as review_created_at,
-        cast(review_answer_timestamp as timestamp) as review_answered_at
+        review_comment_title as title,
+        review_comment_message as message,
+        cast(review_creation_date as timestamp) as created_at,
+        cast(review_answer_timestamp as timestamp) as answered_at
     from source
 )
 
